cbe3d99a8f6a5fcf3e561cc0f12a55057e2168a0
thong-dipro <thongtd@dipro.vn>
2023-09-24 14:41:03 +0700

    feat: add crawed data

--- bin/main.dart
+++ bin/main.dart
@@ -2,12 +2,11 @@ import 'package:craw_svdut/check_account.dart';
 import 'package:craw_svdut/craw_svdut.dart' as craw_svdut;
 import 'package:craw_svdut/models/account.dart';
 import 'package:craw_svdut/read_csv.dart';
-import 'package:craw_svdut/write_txt.dart';
 
 void main(List<String> arguments) {
   print('Hello world: ${craw_svdut.calculate()}!');
   List<Account> accounts = ReadCSV.findMSSV(ReadCSV.readDOB());
   CheckAccount.checkAccount(accounts);
   print(accounts.length);
-  // CheckAccount.checkAccount();
+  // // CheckAccount.checkAccount();
 }
